d3aa01fbbc7634a7518db5085e5f579951d28f9f
francielle oliveira da luz <francielleoliveiradaluz@MacBook-Air-de-francielle.local>
2025-04-13 09:57:28 -0300

    fluxo de programcao reativa

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { LivrosResultado } from '../module/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,12 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes';
   constructor(private httpClientModule: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<any> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.httpClientModule.get(this.API, { params })
+    return this.httpClientModule.get<LivrosResultado>(this.API, { params })
+      .pipe(
+        tap((retornoAPI) => console.log("FLUXO TASP",retornoAPI))
+      )
 
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,21 +1,29 @@
+import { Subscription } from 'rxjs';
 import { LivroService } from './../../service/livro.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca: string = '';
+  subcription: Subscription;
   constructor(private livroService: LivroService) { }
   buscarLivros() {
-    this.livroService.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI),
-      (error)=> console.log(error)
+    this.subcription = this.livroService.buscar(this.campoBusca).subscribe(
+      {
+        next: retornoAPI => console.log('ok'),
+        error: erro => console.log(erro),
+        complete: () => console.log('CONSEGUI')
+      }
     );
+  }
+  ngOnDestroy(){
+    this.subcription.unsubscribe()
   }
 }
 
